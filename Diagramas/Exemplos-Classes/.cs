3122f1b3c1496f5ab9c58f5ec2f92a006cf8051f
niczsouza <nicolasfacheris3@gmail.com>
2026-02-05 08:30:28 -0300

    Update .cs

--- Diagramas/Exemplos-Classes/.cs
+++ Diagramas/Exemplos-Classes/.cs
@@ -1,1 +1,23 @@
+// Abstração de um brinquedo eletrônico
+public class CarrinhoControleRemoto 
+{
+    // ATRIBUTOS (O que o brinquedo TEM)
+    public string Cor;
+    public int Velocidade;
+    public int NivelBateria;
+
+    // MÉTODOS (O que o brinquedo FAZ)
+    public void Acelerar() 
+    {
+        if (NivelBateria > 0) {
+            this.Velocidade += 10;
+            this.NivelBateria -= 1; // Ação consome energia
+        }
+    }
+
+    public void Buzinar() 
+    {
+        Console.WriteLine("Beep! Beep!");
+    }
+}
 
